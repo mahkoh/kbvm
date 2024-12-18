649f0f3866e39bd81e84ab19b4a8b66fd90386e1
Julian Orth <ju.orth@gmail.com>
2024-12-18 21:22:24 +0100

    a

--- compile-tests/src/main.rs
+++ compile-tests/src/main.rs
@@ -18,7 +18,7 @@ use {
     thiserror::Error,
 };
 
-// const SINGLE: Option<&str> = Some("t0004_include");
+// const SINGLE: Option<&str> = Some("t0004");
 const SINGLE: Option<&str> = None;
 const WRITE_MISSING: bool = true;
 const WRITE_FAILED: bool = true;
