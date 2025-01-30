334b47501d1083df9a9bdab7a78e47f28252489d
Peter Hutterer <peter.hutterer@who-t.net>
2025-01-30 12:34:50 +1000

    kbvm: slightly improve the build.rs script
    In both cases fixed here we can generate our output with a single
    expression instead of mutable variables. Functionality is unchanged.

--- kbvm/build.rs
+++ kbvm/build.rs
@@ -9,26 +9,21 @@ use {
 };
 
 fn main() {
-    let mut default_include_dir = None;
-    if default_include_dir.is_none() {
-        if let Ok(var) = get_variable("xkeyboard-config", "xkb_base") {
-            default_include_dir = Some(var);
-        }
-    }
+    let default_include_dir =
+        get_variable("xkeyboard-config", "xkb_base").unwrap_or("/usr/share/X11/xkb".into());
     let mut config = open("config.rs").unwrap();
     writeln!(
         config,
         "pub const DEFAULT_INCLUDE_DIR: &str = \"{}\";",
         default_include_dir
-            .as_deref()
-            .unwrap_or("/usr/share/X11/xkb"),
     )
     .unwrap();
 }
 
 fn open(s: &str) -> io::Result<BufWriter<File>> {
-    let mut path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    path.push(s);
+    let path: PathBuf = vec![env::var("OUT_DIR").unwrap().as_str(), s]
+        .into_iter()
+        .collect();
     Ok(BufWriter::new(
         OpenOptions::new()
             .create(true)
